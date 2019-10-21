2b95824b971004e0b840c463c2301926c83562db
Sue1024 <1589893069@qq.com>
2019-10-21 23:43:53 +0800

    add logout

--- src/Pages/Main/Header/index.js
+++ src/Pages/Main/Header/index.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Menu, Dropdown, Button } from "antd";
+import { Menu, Dropdown, Button, Input } from "antd";
 import axios from "axios";
-import { withRouter } from "react-router"
+import { withRouter } from "react-router";
 import "./index.css";
+import jwt from "jsonwebtoken";
 class Header extends React.Component {
   constructor() {
     super();
-    this.onLogout = this.onLogout.bind(this);
     this.state = {
-        username: ''
-    }
-    this.onLogin = this.onLogin.bind(this)
+      username: ""
+    };
   }
-  onLogin () {
-      this.props.history.push('/sign_in')
-  }
-  onLogout() {
+  onLogin = () => {
+    this.props.history.push("/sign_in");
+  };
+  onRegister = () => {
+    this.props.history.push("/sign_up");
+  };
+  onLogout = () => {
     const instance = axios.delete(
       "http://192.168.3.3:8080/user-service/user/logout",
       {
@@ -24,27 +26,81 @@ class Header extends React.Component {
         }
       }
     );
-    instance.then(response => {
+    instance.then(
+      response => {
         if (response.status === 200 || response.status === 401) {
           localStorage.setItem("access_token", null);
           localStorage.setItem("refresh_token", null);
           this.setState({
-              username: ''
-          })
+            username: ""
+          });
         }
-      });
-  }
+      },
+      error => {
+        console.log(error);
+        if (error.toString().includes("401")) {
+          axios.post(
+            "http://192.168.3.3:8080/user-service/auth-management/update-token",
+            null,
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+              },
+
+              transformRequest: [
+                function(data) {
+                  let ret = "";
+                  for (let it in data) {
+                    ret +=
+                      encodeURIComponent(it) +
+                      "=" +
+                      encodeURIComponent(data[it]) +
+                      "&";
+                  }
+                  return ret;
+                }
+              ],
+
+              data: {
+                refreshToken: localStorage.getItem("refresh_token")
+              }
+            }
+          );
+        }
+        localStorage.setItem("access_token", null);
+        localStorage.setItem("refresh_token", null);
+        this.setState({
+          username: ""
+        });
+      }
+    );
+  };
   componentDidMount() {
     let access_token = localStorage.getItem("access_token");
-    if(access_token) {
-        this.setState({
-            username: "Sue",
-        })
+    if (access_token) {
+      this.setState({
+        username: jwt.decode(access_token) && jwt.decode(access_token).username
+      });
     } else {
-        this.setState({
-            username: ''
-        })
+      this.setState({
+        username: ""
+      });
     }
+  }
+  componentWillReceiveProps() {
+    let access_token = localStorage.getItem("access_token");
+    setTimeout(() => {
+      if (access_token) {
+        this.setState({
+          username:
+            jwt.decode(access_token) && jwt.decode(access_token).username
+        });
+      } else {
+        this.setState({
+          username: ""
+        });
+      }
+    });
   }
   render() {
     const menu = (
@@ -57,14 +113,39 @@ class Header extends React.Component {
     return (
       <div className="header">
         <div className="logo">Easy Blog</div>
-        { this.state.username ? 
-        <Dropdown overlay={menu} placement="bottomLeft">
-          <Button className="user">{this.state.username}</Button>
-        </Dropdown>
-        : <Button className="user" onClick={this.onLogin}>登录</Button>
-        }
+        <div className="center">
+          <Input.Search
+            placeholder="搜索"
+            onSearch={value => console.log(value)}
+            className="input-wrapper"
+          />
+        </div>
+        <div className="right">
+          {this.state.username ? (
+            <Dropdown overlay={menu} placement="bottomLeft">
+              <Button className="user">{this.state.username}</Button>
+            </Dropdown>
+          ) : (
+            <React.Fragment>
+              <Button className="user" onClick={this.onLogin}>
+                登录
+              </Button>
+              <Button className="register" onClick={this.onRegister}>
+                注册
+              </Button>
+            </React.Fragment>
+          )}
+          <Button
+            type="primary"
+            shape="round"
+            icon="pic-right"
+            className="write"
+          >
+            写文章
+          </Button>
+        </div>
       </div>
     );
   }
 }
-export default withRouter(Header)
+export default withRouter(Header);
